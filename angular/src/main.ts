933158ec76f620c21a7ed75b600a2b3f1e3923cf
Ashish Doneriya <ashish.doneriya@gmail.com>
2016-07-29 18:39:40 +0530

    changes

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -7,3 +7,4 @@ if (environment.production) {
 }
 
 bootstrap(AppComponent);
+
--- angular/src/system-config.ts
+++ angular/src/system-config.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 // SystemJS configuration file, see links for more information
 // https://github.com/systemjs/systemjs
 // https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
